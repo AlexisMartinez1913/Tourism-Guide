234e7b9583e7646fbe8ff381ea36074a3f205123
Alexis Martinez <jhonyalexis100@gmail.com>
2023-08-08 10:57:12 -0500

    validaciones local finish

--- src/main/java/org/example/Validaciones/LocalValidacion.java
+++ src/main/java/org/example/Validaciones/LocalValidacion.java
@@ -1,12 +1,24 @@
 package org.example.Validaciones;
 
-public class LocalValidacion {
-    public Boolean validarNit(Integer nit){
-        
+import org.example.Utilidades.Util;
 
+public class LocalValidacion {
+    private Util util = new Util();
+    public Boolean validarNit(String nit) throws Exception{
+        String regexNumeros = "^[0-9]+$";
+        if(!util.buscarCoincidencia(nit,regexNumeros)){
+            throw new Exception("El NIT debe contener ser solo numeros");
+        }
+        if(nit.length()!=10){
+            throw new Exception("El campo nit debe tener  10 dígitos");
+        }
         return true;
+
     }
-    public Boolean valdarNombreEmpresa(String nombre){
+    public Boolean valdarNombreEmpresa(String nombre) throws Exception{
+        if(nombre.length()>30){
+            throw new Exception("El nombre de la empresa no puede sobrepasar los 30 caracteres");
+        }
         return true;
     }
 }
