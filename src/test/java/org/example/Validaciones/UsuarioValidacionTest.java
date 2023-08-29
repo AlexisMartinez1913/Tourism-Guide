e6918a35fa8107e6a467c9a0e67593b58a4d52f3
Alexis Martinez <jhonyalexis100@gmail.com>
2023-08-29 11:46:10 -0500

    pruebas usuario

--- src/test/java/org/example/Validaciones/UsuarioValidacionTest.java
+++ src/test/java/org/example/Validaciones/UsuarioValidacionTest.java
@@ -1,5 +1,7 @@
 package org.example.Validaciones;
 
+import org.example.Utilidades.Mensaje;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -15,17 +17,43 @@ class UsuarioValidacionTest {
     }
     @Test
     public void validarNombreFallaNombreCorto(){
+        //preparar prueba
+        String nombrePrueba = "AlexisG";
+        //ejecutar
+        Exception respuesta = Assertions.assertThrows(Exception.class, ()->
+                this.usuarioValidacion.validarNombre(nombrePrueba));
+        Assertions.assertEquals(Mensaje.LONGITUD_NOMBRE.getMensaje(), respuesta.getMessage());
+
+        //verificar
 
     }
     @Test
     public void validarNombreFallaNombreCaracteres(){
+        String nombrePrueba = "12";
+        Exception respuesta = Assertions.assertThrows(Exception.class, ()->this.usuarioValidacion.validarNombre(nombrePrueba));
+        Assertions.assertEquals(Mensaje.FORMATO_NOMBRE.getMensaje(), respuesta.getMessage());
 
     }
     @Test
     public void validarNombreCorrecto(){
+        String nombrePrueba = "AlexisGarcia";
+        boolean respuesta = Assertions.assertDoesNotThrow(()->this.usuarioValidacion.validarNombre(nombrePrueba));
+        Assertions.assertTrue(respuesta);
 
     }
-    //
+    @Test
+    public void validarCorreoCorrecto(){
+        String correoPrueba = "jhonyalexis100@gmail.com";
+        boolean respuesta = Assertions.assertDoesNotThrow(()->this.usuarioValidacion.validarCorreo(correoPrueba));
+        Assertions.assertTrue(respuesta);
+    }
+    @Test
+    public void validarCorreoIncorrecto(){
+        String correoPrueba = "alexiscom";
+        Exception respuesta = Assertions.assertThrows(Exception.class, ()->this.usuarioValidacion.validarCorreo(correoPrueba));
+        Assertions.assertEquals(Mensaje.FORMATO_CORREO.getMensaje(), respuesta.getMessage());
+    }
+
 
 
 }
